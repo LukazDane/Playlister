3d4914718fee9af5bdf290bd5bd22b3d2fd2a237
Luke <deandrevidal@aol.com>
2019-09-27 11:23:39 -0700

    recursively removed cache files

--- app.py
+++ app.py
@@ -48,5 +48,28 @@ def playlists_show(playlist_id):
     return render_template('playlists_show.html', playlist=playlist)
 
 
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    video_links = '\n'.join(playlist.get('videos'))
+    return render_template('playlists_edit.html', playlist=playlist)
+
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    """Submit an edited playlist."""
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split(),
+        'ratings': request.form.get('ratings')
+    }
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
